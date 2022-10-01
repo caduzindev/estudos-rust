cacd28183c360691d4394b8ad757fb30dedbba16
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2022-10-01 19:01:43 -0300

    fix: deleted

--- caixa/target/debug/caixa.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Lenovo\projects\estudos-rust\caixa\target\debug\caixa.exe: C:\Users\Lenovo\projects\estudos-rust\caixa\src\main.rs
--- caixa/target/debug/deps/caixa.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Lenovo\projects\estudos-rust\caixa\target\debug\deps\caixa.exe: src\main.rs
-
-C:\Users\Lenovo\projects\estudos-rust\caixa\target\debug\deps\caixa.d: src\main.rs
-
-src\main.rs:
