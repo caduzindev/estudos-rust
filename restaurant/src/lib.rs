25ae1939c2b1b47e5e4f6f623f991c82729298e0
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2022-10-12 22:18:58 -0300

    feat: modulariação

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,12 +1,9 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
+mod front_of_house;
 
-    mod serving {
-        fn take_order() {}
-        fn server_order() {}
-        fn take_payment() {}
-    }
+pub fn eat_at_restaurant() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
