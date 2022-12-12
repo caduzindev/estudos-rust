b79c25130c779759e2ecdc9dbdcf0e11e965f2b6
Carlos Eduardo Almeida Silva <cadudevmaster@gmail.com>
2022-12-11 21:10:15 -0300

    feat: modularização e tdd adicionado no projeto minegrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,7 @@
 use minigrep::Config;
 use std::env;
 use std::process;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
